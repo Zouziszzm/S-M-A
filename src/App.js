813b750b361d8d1c59bd967aa02345870493d373
Zouziszzm <uaepeekawariour@gmaill.com>
2023-01-18 23:06:18 +0530

    m-text removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import {User} from './users.js';
 function App() {
-    const users= [{name:"farhaan" ,age: 23}];
+    const users= [{name:"farhaan" ,age: 23, planet:true}];
     return(
         <div className="App">
         {users.map((user, key) => {
-            return<User name={user.name} age={user.age}/>
+          if(user.planet)  return<User name={user.name} age={user.age}/>
         })}
         </div>
     );
